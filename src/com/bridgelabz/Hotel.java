41c7d026a4a3c696c6cb45051eec32bae0924eb4
Ravi@036# <admin@admins-MacBook-Pro.local>
2023-05-10 22:04:23 +0530

    Added: Stream_API

--- src/com/bridgelabz/Hotel.java
+++ src/com/bridgelabz/Hotel.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 public class Hotel {
@@ -11,10 +12,6 @@ public class Hotel {
     int weekdayRateForRewardCustomer;
     int weekendRateForRewardCustomer;
 
-    public Hotel() {
-
-    }
-
     public Hotel(String hotelName, int hotelRating, int weekdayRateForRegularCustomer, int weekendRateForRegularCustomer, int weekdayRateForRewardCustomer, int weekendRateForRewardCustomer) {
         this.hotelName = hotelName;
         this.hotelRating = hotelRating;
@@ -24,7 +21,25 @@ public class Hotel {
         this.weekendRateForRewardCustomer = weekendRateForRewardCustomer;
     }
 
-    public void findPrice(LocalDate startDate, LocalDate endDate, Boolean customerType) {
+    public static int getWeekendDaysInRange(LocalDate startDate, LocalDate endDate) {
+        return (int) startDate.datesUntil(endDate.plusDays(1)).filter(date->date.getDayOfWeek().equals(DayOfWeek.SATURDAY) ||date.getDayOfWeek().equals(DayOfWeek.SUNDAY)).count();
+    }
+    public static int getWeekDaysInRange(LocalDate startDate, LocalDate endDate) {
+        return (int) startDate.datesUntil(endDate.plusDays(1)).filter(date->date.getDayOfWeek().equals(DayOfWeek.MONDAY) ||date.getDayOfWeek().equals(DayOfWeek.TUESDAY) ||date.getDayOfWeek().equals(DayOfWeek.WEDNESDAY) ||date.getDayOfWeek().equals(DayOfWeek.THURSDAY) ||date.getDayOfWeek().equals(DayOfWeek.FRIDAY)).count();
+    }
 
+    public int findPrice(Boolean customerType, int numberOfWeekDays, int numberOfWeekendDays) {
+        if (customerType == false) {
+            int weekDayPriceForRegularCustomer = weekdayRateForRegularCustomer * numberOfWeekDays;
+            int weekEndDayPriceForRegularCustomer = weekendRateForRegularCustomer * numberOfWeekendDays;
+            int totalPrice = weekDayPriceForRegularCustomer + weekEndDayPriceForRegularCustomer;
+            return totalPrice;
+        }
+        else {
+            int weekDayPriceForRegularCustomer = weekdayRateForRewardCustomer * numberOfWeekDays;
+            int weekEndDayPriceForRegularCustomer = weekendRateForRewardCustomer * numberOfWeekendDays;
+            int totalPrice = weekDayPriceForRegularCustomer + weekEndDayPriceForRegularCustomer;
+            return totalPrice;
+        }
     }
 }
--- src/com/bridgelabz/HotelReservationSystemMain.java
+++ src/com/bridgelabz/HotelReservationSystemMain.java
@@ -1,6 +1,8 @@
 package com.bridgelabz;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 public class HotelReservationSystemMain {
     public static void main(String[] args) {
@@ -9,20 +11,38 @@ public class HotelReservationSystemMain {
         LocalDate startDate = LocalDate.parse(sc.nextLine(), DateTimeFormatter.ofPattern("dd-MM-yyyy"));
         System.out.println("Enter the enddate dd-MM-yyyy: ");
         LocalDate endDate = LocalDate.parse(sc.nextLine(),DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-        System.out.println("Enter the customerTypes: ");
+        System.out.println("Enter the customerType: ");
         Boolean customerType = sc.nextBoolean();
-        //LocalDate date.getDayOfWeek();
-
+        System.out.println();
         Hotel lakeWood = new Hotel("LakeWood", 3, 110, 90, 80,80);
 
         Hotel bridgeWood = new Hotel("BridgeWood", 4, 160, 60, 110,50);
 
         Hotel ridgeWood = new Hotel("RidgeWood", 5, 220, 150, 100,40);
-        //hotel.findPrice(startDate, endDate, customerType);
 
-        private int getWeekendDaysInRange(LocalDate startDate, LocalDate endDate) {
-            return (int) startDate.datesUntil(endDate.plusDays(1)).filter(date->date.getDayOfWeek().equals(DayOfWeek.SATURDAY) ||date.getDayOfWeek().equals(DayOfWeek.SUNDAY)).count();
+        int numberOfWeekDays = Hotel.getWeekDaysInRange(startDate, endDate);
+        int numberOfWeekEndDays = Hotel.getWeekendDaysInRange(startDate, endDate);
+
+        int lakeWoodPrice = lakeWood.findPrice(customerType, numberOfWeekDays, numberOfWeekEndDays);
+        System.out.println("LakeWood Hotel Price is " + lakeWoodPrice);
+        int bridgeWoodPrice = bridgeWood.findPrice(customerType, numberOfWeekDays, numberOfWeekEndDays);
+        System.out.println("BridgeWood Hotel Price is " + bridgeWoodPrice);
+        int ridgeWoodPrice = ridgeWood.findPrice(customerType, numberOfWeekDays, numberOfWeekEndDays);
+        System.out.println("RidgeWood Hotel Price is " + ridgeWoodPrice);
+        System.out.println();
+
+        List<Integer> hotelPrices = Arrays.asList(lakeWoodPrice,bridgeWoodPrice,ridgeWoodPrice);
+
+        int cheapHotelPrice = hotelPrices.stream().min(Integer::compare).get();
+
+        if (lakeWoodPrice == cheapHotelPrice) {
+            System.out.println("LakeWood is the Cheapest Hotel");
         }
-
+        else if (bridgeWoodPrice == cheapHotelPrice) {
+            System.out.println("BridgeWood is the Cheapest Hotel");
+        }
+        else {
+            System.out.println("RidgeWood is the Cheapest Hotel");
+        }
     }
 }
